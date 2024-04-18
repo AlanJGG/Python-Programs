44438a796264df2f4dd6ca0e0afdf859c9193162
AlanJGG <alanguzmangutierrez2005@gmail.com>
2024-04-17 19:28:07 -0600

    refactoring code

--- 001_MatrizInversa/main.py
+++ 001_MatrizInversa/main.py
@@ -4,13 +4,17 @@ dim = int(input("Ingresa la dimensión de la matriz: "))
 
 mat = llenarMatriz(dim)
 matIdentidad = llenarMatrizIdentidad(dim)
+
 if tieneInversa(mat):
     print("\n--Matriz Inicial e Identidad--\n")
     imprimirMatrices(mat, matIdentidad, dim)
+
     gaussJordan(mat, matIdentidad, dim)
+
     print("\n--Matriz Resultante e Inversa--\n")
     #imprimirMatrices(mat, matIdentidad, dim)
     imprimirMatrices(flotanteAFraccion(mat, dim), flotanteAFraccion(matIdentidad, dim), dim)
+    
 else:
     print("\nLa matriz no tiene inversa\n")
             
